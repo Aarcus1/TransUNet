31e3f59896558133f53eb02611c905778cd539b5
Aarcus <37001141+Aarcus1@users.noreply.github.com>
2025-10-09 18:17:09 +0200

    Try fixing model loading during test time

--- test.py
+++ test.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
 
     snapshot = os.path.join(snapshot_path, 'best_model.pth')
     if not os.path.exists(snapshot): snapshot = snapshot.replace('best_model', 'epoch_'+str(args.max_epochs-1))
-    net.load_state_dict(torch.load(snapshot))
+    net.load_state_dict(torch.load(snapshot)['model'])
     snapshot_name = snapshot_path.split('/')[-1]
 
     log_folder = './test_log/test_log_' + args.exp
